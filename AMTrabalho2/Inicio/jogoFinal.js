ea735c0d62708cbaff8bf70b413d81979b118fb1
DaveFux <33520145+DaveFux@users.noreply.github.com>
2018-06-18 13:17:14 +0100

    l

--- AMTrabalho2/Inicio/jogoFinal.js
+++ AMTrabalho2/Inicio/jogoFinal.js
@@ -285,7 +285,7 @@
                 for (mob of osMobs) {
                     if (Math.abs(torre.x - mob.x) < (torre.range * 46) && Math.abs(torre.y - mob.y) < (torre.range * 46)) {
                         torre.attack(mob, function () {
-                            var umaBala = new Bala(gSpriteSheets['assets//tank.png'], torre.x, torre.y + 5, torre.);
+                            var umaBala = new Bala(gSpriteSheets['assets//tank.png'], torre.x, torre.y + 5, torre.type, torre.damage, torre.speed, torre.range, torre.special);
                             umaBala.scaleFactor = 0.3;
                             umaBala.vy = 4;
                             umaBala.id = Date.now();
