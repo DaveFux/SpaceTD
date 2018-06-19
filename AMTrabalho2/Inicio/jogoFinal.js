9c91a497e2caee4911bc53e2ae887ac96f10a658
DaveFux <33520145+DaveFux@users.noreply.github.com>
2018-06-19 15:00:49 +0100

    2018-06-19 15:00

--- AMTrabalho2/Inicio/jogoFinal.js
+++ AMTrabalho2/Inicio/jogoFinal.js
@@ -374,7 +374,7 @@
            if (tiles[i].hitTestPoint(point.x, point.y)){
                canvasses.tiles.ds.clearRect(tiles[i].x,tiles[i].y,tiles[i].width,tiles[i].height);
               tiles[i].drawColisionBoundaries(canvasses.tiles.ds, true, false, "#31FF00", "red");
-              canvas.addEventListener("onclick",criarObjeto(tiles[i]), false);
+           //   canvas.addEventListener("onclick",criarObjeto(tiles[i]), false);
            }
        }
        checkColisions(); // Verificar se h� colis�es
